e5525240120cca87d4a0d48d97fb4da0290a8f14
Simone Terranova <168133698+simoneterranova@users.noreply.github.com>
2025-03-09 09:46:23 +0100

    prova2 come hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,7 +37,7 @@ const Hero = () => {
       id="home" 
       className="relative min-h-screen flex items-center justify-center overflow-hidden"
       style={{
-        backgroundImage: "url('https://livesicilia.it/wp-content/uploads/2022/03/qub3-Copia.jpg')",
+        backgroundImage: "url('/public/prova2.png')",
         backgroundSize: 'cover',
         backgroundPosition: 'center'
       }}
